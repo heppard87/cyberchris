ab5402b46154d17e2e9fe32bd62fd4ce4c54b769
chrisheppard <cheppard03@gmail.com>
2026-02-02 07:04:11 -0800

    Change base URL to '/cyberchris/'
    Updated base URL for deployment to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Change this to '/' if you are using a custom domain or heppard87.github.io
-  // Use '/cyberchris/' if your URL is heppard87.github.io/cyberchris/
-  base: '/', 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // This must match your repository name exactly
+  base: '/cyberchris/', 
 })
